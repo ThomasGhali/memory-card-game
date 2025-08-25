b03ca29f23ee9483d8982e921ffefb78ad683ab8
Thomas <thomasmetias29@gmail.com>
2025-08-25 17:37:25 +0300

    feat: add yugi & orichalcos ring animation at menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,19 @@
+import '../styles/Menu.css'
+import yugi from '../assets/yugi.png'
+import demo from '../assets/demo2.png'
+import ori from '../assets/orichalcos.png'
+
 export default function Menu() {
   return (
-    <></>
+    <div className="container">
+      <div className="empty"></div>
+      <div className="menu-window">
+        {/* <img src={demo} alt="" width="500px"/> */}
+      </div>
+      <div className="yugi-container">
+        <img src={yugi} alt="yugi" className='yugi' />
+        <img src={ori} alt="orichalcos ring" className='ori' />
+      </div>
+    </div>
   )
 }
