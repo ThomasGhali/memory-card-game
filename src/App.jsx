23c22962801115cddd22a4deae83aaf208a2507b
Thomas <thomasmetias29@gmail.com>
2025-08-26 10:58:00 +0300

    feat: add change level logic in menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Menu from "./components/Menu"
 
 export default function App() {
-  const [gameLevel, setGameLevel] = useState({level: 'Easy', order: '1', text: "Duelist’s Awakening"})
+  const [gameLevel, setGameLevel] = useState({level: 'Easy', order: 1, text: "Cardboard Chaos"})
 
   return(
     <>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,37 +8,56 @@ import entranceMessage from './EntranceMessages'
 
 
 export default function Menu({ gameLevel, setGameLevel }) {
+  const currentLevelIndex = gameLevel.order - 1;
+  const levels = [{level: 'Easy', order: 1, text: "Cardboard Chaos"}, {level: 'Mid', order: 2, text: "Deck of Doom"}, {level: 'Hard', order: 3, text: "Ring of Ridiculousness"}]
+  const numberOfLevels = levels.length;
+
+  function changeLevel(nextLevelIndex) {
+    const newIndex = (currentLevelIndex + nextLevelIndex) % numberOfLevels;
+
+    setGameLevel({...levels[newIndex]});
+  }
 
   return (
     <div className="container">
       <div className="empty"></div>
       <div className="menu-window">
-        <img src={logo} className='logo' />
+        <img src={logo} className="logo" />
         <div className="menu-img-wrapper">
-          <img src={menu} className='menu-img' />
+          <img src={menu} className="menu-img" />
           <div className="menu-buttons-wrapper">
-            <button aria-label="Start Game" className='menu-btn'>
+            <button aria-label="Start Game" className="menu-btn">
               <img src={menuBtns} aria-hidden="true" />
-              <span className='menu-btn__text'>Start Game</span>
+              <span className="menu-btn__text">Start Game</span>
             </button>
-            <button aria-label="Start Game" className='menu-btn'>
+            <button aria-label="Start Game" className="menu-btn">
               <img src={menuBtns} aria-hidden="true" />
-              <span className='menu-btn__text'>Level {gameLevel.order}: {gameLevel.level}</span>
-              <span className='menu-btn__sub'>{gameLevel.text}</span>
+              <span className="menu-btn__text level">
+                Level {gameLevel.order}: {gameLevel.level}
+              </span>
+              <span 
+                onClick={() => changeLevel(-1)}
+                className="hitbox left-arrow"
+              ></span>
+              <span 
+                onClick={() => changeLevel(1)}
+                className="hitbox right-arrow"
+              ></span>
+              <span className="menu-btn__sub">{gameLevel.text}</span>
             </button>
-            <button aria-label="Start Game" className='menu-btn'>
+            <button aria-label="Start Game" className="menu-btn">
               <img src={menuBtns} aria-hidden="true" />
-              <span className='menu-btn__text'>How to play?</span>
+              <span className="menu-btn__text">How to play?</span>
             </button>
           </div>
         </div>
       </div>
       <div className="yugi-container">
-        <img src={yugi} alt="yugi" className='yugi' />
+        <img src={yugi} alt="yugi" className="yugi" />
         <div className="Entrance-message">
           {entranceMessage()}
         </div>
-        <img src={ori} alt="orichalcos ring" className='ori' />
+        <img src={ori} alt="orichalcos ring" className="ori" />
       </div>
     </div>
   )
