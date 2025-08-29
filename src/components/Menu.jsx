0490bc3847d4c81eabf9f2e3713bdfb773992eea
Thomas <thomasmetias29@gmail.com>
2025-08-29 22:39:04 +0300

    feat: add music controls with logic for menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -82,7 +82,44 @@ export default function Menu({
     <div className="menu-container">
       <div className="empty"></div>
       <div className="menu-window">
-        <img src={logo} className="logo" />
+        <div className="logo-controls-wrapper">
+          <img src={logo} className="logo" />
+          <div className="controls-wrapper">
+          <span className={`sound ${soundIsMuted ? 'music-muted' : ''}`}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                onClick={() => setSoundIsMuted(prev => !prev)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+                />
+              </svg>
+          </span>
+            <span className={`sound ${musicIsMuted ? 'music-muted' : ''}`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                onClick={() => setMusicIsMuted(prev => !prev)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"
+                />
+              </svg>
+            </span>
+
+          </div>
+        </div>
         <div className="menu-img-wrapper">
           <img src={menu} className="menu-img" />
           <div className="menu-buttons-wrapper">
